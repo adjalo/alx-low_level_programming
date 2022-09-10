b07504afa4d21a9f708b306352e28e6b405f9886
adjalo <adamujalo@gmail.comgit config --global user.email adamujalo@gmail.com>
2022-09-09 17:56:28 -0700

    h

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,61 +1,51 @@
+#include <stdio.h>
+
 #include <stdlib.h>
 
 #include <time.h>
-
-#include <stdio.h>
 
 
 
 /**
  *
- *  * main - assign a random number to the variable n each time it is executed
+ *  * main - Entry point
  *
- *   * if the number is greater than 0: is positive, if the number is 0: is zero
+ *   *
  *
- *    * if the number is less than 0: is negative, followed by a new line
+ *    * Return: Always 0 (Success)
  *
- *     * Return: 0
- *
- *      */
-
-
+ *     */
 
 int main(void)
 
-{
-
-	int n;
 
 
+		int n, d;
 
-	srand(time(0));
 
-	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+			srand(time(0));
 
-	{
+				n = rand() - RAND_MAX / 2;
 
-		printf("%d is positive\n", n);
+					d = n % 10;
 
-	}
 
-	else if (n < 0)
 
-	{
+						printf("Last digit of %d is %d and is ", n, d);
 
-		printf("%d is negative\n", n);
+							if (d > 5)
 
-	}
+			printf("greater than 5\n");
 
-	else
+				else if (d == 0)
 
-	{
+			printf("0\n");
 
-		printf("%d is zero\n", n);
+				else
 
-	}
+						printf("less than 6 and not 0\n");
 
-	return (0);
+							return (0);
 
-}
+							}
