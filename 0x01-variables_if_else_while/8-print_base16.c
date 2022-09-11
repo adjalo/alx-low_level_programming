a5fb5c118862ab35878edb0458e816c9fc4d5279
adjalo <adamujalo@gmail.comgit config --global user.email adamujalo@gmail.com>
2022-09-11 15:40:31 -0700

    kkkk

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,12 +5,14 @@ Return: Always 0 (Success/correct)
 int main(void)
 {
 int i;
-char j;
 
+char j;
 for (i = 48; i < 58; i++)
 {
 putchar(i);
+
 }
+
 for (j = 'a'; j <= 'f'; j++)				{
 putchar(j);
 }
