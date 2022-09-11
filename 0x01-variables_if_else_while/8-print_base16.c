8b59914dc91e91f276105f42f1008236c296c24f
adjalo <adamujalo@gmail.comgit config --global user.email adamujalo@gmail.com>
2022-09-11 14:47:21 -0700

    fff

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point   
- * Return: Always 0 (Success/correct)
+ * main - Entry point
+ *  Return: Always 0 (Success/correct)
  */
 
 int main(void)
+
 {
 
 int i;
@@ -14,11 +15,10 @@ for (i = 48; i < 58; i++)
 {
 putchar(i);
 }
-for (j = 'a'; j <= 'f'; j++)
+for (j = 'a'; j <= 'f'; j++)	
 {
 putchar(j);
 }
 putchar('\n');
 return (0);
 }
-					{
