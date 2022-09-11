b4fdf1e92d92412be03dc0058436f35a5a7e62e6
adjalo <adamujalo@gmail.comgit config --global user.email adamujalo@gmail.com>
2022-09-11 11:18:43 -0700

    hg

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 /**
- * main - program that prints all possible different combinations of
- *three digits
- * Numbers must be separated by ,followed by a space
- *The two digits must be different
- *012, 120, 102, 021, 201, 210 are considered the same combination
- *Return: 0
+ *mai - points all single digit numbers of base 10 starting from 0,
+ *followed by a new line,using putchar
+ *Return: always 0 (success)
  */
 int main(void)
 {
-
 int n;
+
 for (n=48; n<58; n++)
 {
 	putchar(n);
