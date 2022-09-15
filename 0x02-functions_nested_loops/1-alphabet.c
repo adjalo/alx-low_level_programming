ac6be22007e350e56ea6d87a1e06f676205d3101
adjalo <adamujalo@gmail.comgit config --global user.email adamujalo@gmail.com>
2022-09-14 20:18:30 -0700

    g

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- *print_alphabet - print alphabet lowercase
- *Return: Always 0. 
- */
+*print_alphabet - print alphabet lowercase
+*Return: Always 0. 
+*/
 void print_alphabet(void)
 {
 char alphabet;
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+for (alphabet ='a'; alphabet <='z'; alphabet++)
 {
 _putchar(alphabet);
 }
