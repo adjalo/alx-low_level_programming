14e92f9ac8893796127c4fd0513d5bcad5261ac4
adjalo <adamujalo@gmail.comgit config --global user.email adamujalo@gmail.com>
2022-09-20 15:45:24 -0700

    c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * swap_int - swaps the values pointed to by arguments
  * @a: pointer argument 1
@@ -8,9 +7,8 @@
 
 void swap_int(int *a, int *b)
 {
-	int c;
+	int con = *a;
 
-	c = *a;
 	*a = *b;
-	b = c;
+	*b = con;
 }
