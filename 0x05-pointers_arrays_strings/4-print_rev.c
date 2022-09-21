462150100d254d0abd3d144a650a9c132325564d
adjalo <adamujalo@gmail.comgit config --global user.email adamujalo@gmail.com>
2022-09-20 20:17:28 -0700

    h

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,16 +4,18 @@
  * print_rev - reverse something 
  * @s: params
  */
+
 void print_rev(char *s)
 
 {
+	
 int i = 0;
 int j;
-
-while (s[i] != '\0')
-{
-i++;										}
-for (j = i - 1; j >= 0; j--)
+	
+	while (s[i] != '\0')
+{	
+	i++;									}	
+	for (j = i - 1; j >= 0; j--)
 {
 										_putchar(s[j]);
 										}
