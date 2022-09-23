7dd62c52339b6aabc667bddf9bbf414edc868c2c
adjalo <adamujalo@gmail.comgit config --global user.email adamujalo@gmail.com>
2022-09-22 18:16:06 -0700

    g

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * rot13 - encodes a string using rot13
  * @s: input string.
@@ -8,43 +7,24 @@
 char *rot13(char *s)
 
 {
-
 	int count = 0, i;
 
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-
-
-	while (*(s + count) != '\0')
+while (*(s + count) != '\0')
 
 	{
-
-
-
 		for (i = 0; i < 52; i++)
-
 		{
-
 			if (*(s + count) == alphabet[i])
-
 			{
-
 				*(s + count) = rot13[i];
-
-				break;
-
-			}
-
-		}
-
-		count++;
-
-	}
-
-
-
-	return (s);
-
+break;
+}
+}
+count++;
+}
+return (s);
 }
