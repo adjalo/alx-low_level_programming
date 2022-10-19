c1e32536d8de9b0e70a297f323088ea35cafd572
adamaujalog <adamaujalog@gmai.com>
2022-10-19 11:38:35 -0700

    message

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,51 +3,46 @@
 
 
 /**
- * jack_bauer -prints every minute of the day
- * Return: Always 0.
+ * print_last_digit - last digit
+*@n: The int to print
+ *Return: Always 0.
  */
 
 
 
-void jack_bauer(void)
+int print_last_digit(int n)
 
 
 
 {
 
-	int a;
-
-	int b;
+	int last_digit;
 
 
 
-	for (a = 0; a <= 23; a++)
+	if (n < 0)
 
 	{
 
-		for (b = 0; b <= 59; b++)
+		last_digit = (-1 * (n % 10));
 
-		{
+		_putchar (last_digit + '0');
 
-			_putchar (a / 10 + '0');
+		return (last_digit);
 
-			_putchar (a % 10 + '0');
-
-			_putchar (':');
-
-			_putchar (b / 10 + '0');
-
-			_putchar (b % 10 + '0');
-
-			_putchar ('\n');
+	}
 
 
 
-		}
+	else
 
+	{
 
+		last_digit = (n % 10);
 
+		_putchar (last_digit + '0');
 
+		return (last_digit);
 
 	}
 
