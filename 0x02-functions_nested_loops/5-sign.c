7a720c1b3e8d22503e46b7d5ac882fbf607529e9
adamaujalog <adamaujalog@gmai.com>
2022-10-19 11:01:06 -0700

    my comment

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,28 +4,29 @@
  * @n: the int to print
  * Return: Always 0.
  */
+
 int print_sign(int n)
 
 {
 
-	if (n > 0)
+if (n > 0)
 
 {
 
-	_putchar ('+');
+_putchar ('+');
 
-	return (1);
+return (1);
 }
 
 
 
-	else if (n == 0)
+else if (n == 0)
 
 {
 
 _putchar ('0');
 
-	return (0);
+return (0);
 
 }
 
@@ -39,7 +40,4 @@ _putchar ('-');
 return (-1);
 
 }
-
-
-
 }
