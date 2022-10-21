e91edf1a340e38b39b76824d9a392fc3dce5e7f3
adamaujalog <adamaujalog@gmai.com>
2022-10-21 14:39:47 -0700

    my commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -39,7 +39,7 @@ void times_table(void)
 
 	if (b != 0)
 
-				{
+	{
 
 	_putchar (' ');
 
@@ -47,7 +47,7 @@ void times_table(void)
 
 	}
 
-		if (b < 9)
+	if (b < 9)
 
 	{
 
@@ -55,20 +55,20 @@ void times_table(void)
 
 	_putchar (' ');
 
-		}
+	}
 
 	}
 
 	else
 
-		{
+	{
 
 	_putchar ((c / 10) + '0');
 	_putchar ((c % 10) + '0');
 
 	if (b < 9)
 
-		{
+	{
 
 	_putchar(',');
 
