810bf6a1d26662dbd66785183e6660a07523ed8d
adamaujalog <adamaujalog@gmai.com>
2022-10-21 14:34:53 -0700

    my commmit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,69 +19,68 @@ void times_table(void)
 
 	{
 
-		for (b = 0; b <= 9; b++)
+	for (b = 0; b <= 9; b++)
 
-		{
+	{
 
-			c = a * b;
+	c = a * b;
 
-			if ((c / 10) == 0)
+	if ((c / 10) == 0)
 
-			{
+	{
 
-				if (b == 0)
+	if (b == 0)
 
-				{
+	{
 
-					_putchar ('0');
+	_putchar ('0');
 
-				}
+	}
 
-				if (b != 0)
-
-				{
-
-					_putchar (' ');
-
-					_putchar ((c % 10) + '0');
-
-				}
-
-				if (b < 9)
+	if (b != 0)
 
 				{
 
-					_putchar(',');
+	_putchar (' ');
 
-					_putchar (' ');
+	_putchar ((c % 10) + '0');
 
-				}
+	}
 
-			}
+		if (b < 9)
 
-			else
+	{
 
-			{
+	_putchar(',');
 
-				_putchar ((c / 10) + '0');
-
-				_putchar ((c % 10) + '0');
-
-				if (b < 9)
-
-				{
-
-					_putchar(',');
-
-					_putchar (' ');
-
-				}
-
-			}
+	_putchar (' ');
 
 		}
 
-		_putchar ('\n');
+	}
+
+	else
+
+		{
+
+	_putchar ((c / 10) + '0');
+	_putchar ((c % 10) + '0');
+
+	if (b < 9)
+
+		{
+
+	_putchar(',');
+
+	_putchar (' ');
+
+	}
+
+	}
+
+	}
+
+	_putchar ('\n');
 
 	}
 
