d9a92f59f87af2fc711cd184463d720a06e5ca6c
adamaujalog <adamaujalog@gmai.com>
2022-10-22 00:13:40 -0700

    my commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,8 +8,6 @@
  *Return: dest
  */
 
-
-
 char *_memset(char *s, char b, unsigned int n)
 
 
@@ -18,22 +16,16 @@ char *_memset(char *s, char b, unsigned int n)
 
 	unsigned int i;
 
+		/*Delacring FOR*/
 
+	for (i = 0; i < n; i++)
+	{
 
-			/*Delacring FOR*/
-
-		for (i = 0; i < n; i++)
-
-				{
-
-				*(s + i) = b; /*add 1 position s*/
+	*(s + i) = b; /*add 1 position s*/
 
 
 
-						} /*END FOR*/
-
-
-
-			return (s);
+	} /*END FOR*/
+		return (s);
 
 }
