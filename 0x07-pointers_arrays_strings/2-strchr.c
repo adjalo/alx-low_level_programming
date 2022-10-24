132122bb621e205c5a060f9382bf0a2e00396053
adamaujalog <adamaujalog@gmai.com>
2022-10-24 08:51:53 -0700

    f

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,42 +13,28 @@ char *_strchr(char *s, char c)
 
 {
 
+	while (*s != '\0') /*Declaring WHILE*/
 
+	{
+	if (*s == c) /*if s ==  c*/
 
-		while (*s != '\0') /*Declaring WHILE*/
+	{
 
-				{
-
-							if (*s == c) /*if s ==  c*/
-
-										{
-
-														return (s); /*return s*/
+return (s); /*return s*/
 
 																}
+++s;
+
+	}
+	if (*s == c)
+
+{
+									
+return (s);
 
 
 
-									++s;
-
-										}
-
-
-
-			if (*s == c)
-
-					{
-
-								return (s);
-
-
-
-									}
-
-
-
-
-
-				return (0);/* values null*/
+}
+return (0);/* values null*/
 
 }
